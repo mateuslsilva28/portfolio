aa0ccbbc2beb16730decf69ba3254929946cecd7
Mateus <mateuslsilva28@gmail.com>
2020-06-20 05:47:04 -0300

    C4 - Adicionando menu mobile

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
 const ilustracao1 = $("#imgApresentacao")
+const menu = $(".hamburguer");
+const mobile = $(".menuMobile");
+const fechar = $(".close")
+let largura, altura;
+
+setInterval(() => {
+    largura = $(window).width();
+    altura = $(window).height();
+    menu.click(function(){
+        mobile.css({"width":largura,"height":altura})
+        mobile.css({"top":"0%"})
+    })
+    fechar.click(function(){
+        mobile.css({"top":"-700%"})
+    })    
+}, 700);
 
 setTimeout(function(){
     $(".textoApresentacao").css("top", "0")
